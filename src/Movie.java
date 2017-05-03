f153e67a6def920b7bf9eebc59fa84e6dcdfd57d
Sven Baumann <svenmbaumann@web.de>
2017-05-03 10:17:29 +0200

    Tests hinzugefügt

--- src/Movie.java
+++ src/Movie.java
@@ -3,46 +3,38 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
-    private Price price;
-
-    public Movie(String newtitle, int priceCode) {
+    private Price _price;
+    
+    public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        setPriceCode(priceCode);
+        setPriceCode(newpriceCode);
     }
-
     public int getPriceCode() {
-        return price.getPriceCode();
+        return _price.getPriceCode();
     }
-
     public void setPriceCode(int arg) {
-        switch (arg) {
-            case REGULAR:
-                price = new RegularPrice();
-                break;
-            case CHILDRENS:
-                price = new ChildrensPrice();
-                break;
-            case NEW_RELEASE:
-                price = new NewReleasePrice();
-                break;
-            default:
-                throw new IllegalArgumentException("Incorrect Price Code");
+        switch (arg){
+        	case REGULAR:
+        		_price = new RegularPrice();
+        		break;
+        	case CHILDRENS:
+        		_price = new ChildrensPrice();
+        		break;
+        	case NEW_RELEASE:
+        		_price = new NewReleasePrice();
+        		break;
+        	default:
+        		throw new IllegalArgumentException("Incorrect Price Code");
         }
     }
-
     public String getTitle (){
         return title;
     }
-
-    public double getCharge(int daysRented) {
-        return price.getCharge(daysRented);
+    double getCharge(int daysRented) {
+    	return _price.getCharge(daysRented);
     }
-
-    public int getFrequentRenterPoints( int daysRented) {
-        return price.getFrequentRenterPoints(daysRented);
-
+    public int getFrequentRenterPoints(int daysRented){
+    	return _price.getFrequentRenterPoints(daysRented);
     }
-
-
+    
 }
